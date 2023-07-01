5a4f36c06d6f8f11c352c2173ec19f351e2ac9ee
Mark Corder <micorder@proton.me>
2023-06-30 18:45:41 -0600

    Added loop to loop through each line in text file and ffmpeg command to split and create audio files.

--- ffmpegSplit.ps1
+++ ffmpegSplit.ps1
@@ -3,13 +3,12 @@ $audioFile = Read-Host -Prompt 'Input path to audio file to split'
 $segmentFile = Read-Host -Prompt 'Input path to text file with end of segment time and title separated by " "'
 
 # Read in segment file
-$fileData = Get-Content $segmentFile
-$dataArray = $fileData.Split(" ")
-$startTime = $dataArray[0]
-$endTime = $dataArray[1]
-$title = $dataArray[2]
-
-$startTime
-$endTime
-$title
-
+Get-Content $segmentFile | ForEach-Object {
+    $dataArray = $_.Split(" ")
+    $startTime = $dataArray[0]
+    $endTime = $dataArray[1]
+    $title = $dataArray[2]
+    $title = $title + ".mp3"
+    
+    ffmpeg -ss $startTime -to $endTime -i $audioFile -c copy $title
+}
